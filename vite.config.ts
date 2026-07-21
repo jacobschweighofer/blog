f6dd5b096e90333cea20e76bcd8ff469388d898a
Jacob Schweighofer <focusedanondev@gmail.com>
2026-07-21 19:28:31 +0300

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,16 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/blog/",
+
   plugins: [tailwindcss()],
+
   server: {
-    // 1. Forces Vite to use IPv4 instead of letting VS Code get confused by IPv6 localhost
     host: "127.0.0.1",
     port: 5173,
 
-    // 2. Prevents the file watcher from choking on node_modules
     watch: {
       ignored: ["**/node_modules/**"],
-      // If you are using WSL (Windows Subsystem for Linux),
-      // uncomment the line below to force file polling:
-      // usePolling: true,
     },
   },
 });
